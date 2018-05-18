616ee764b02a2f8a58c8710747b0510e76e6fea3
Pegas-7 <ifk88@mail.ru>
2018-05-18 23:51:15 +0300

    Done task4

---
+++ src/main/java/task4/Task4.java
@@ -0,0 +1,30 @@
+package task4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static helpers.HelperMethods.readString;
+
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println("Enter the string, than press \"Enter\"");
+
+        List<String> list = getListWordsFromString(readString());
+
+        System.out.println("Amount of words " + list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    public static List<String> getListWordsFromString(String s) {
+        List<String> list1 = Arrays.asList(s.replaceAll(",", "").split("\\s"));
+        List<String> list2 = new ArrayList<String>();
+
+        for (int i = 0; i < list1.size(); i++) {
+            if (!list1.get(i).equals("")) list2.add(list1.get(i));
+        }
+        return list2;
+    }
+}
