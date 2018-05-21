56cc8972bb36ce9c498a0dc2d0fb1f8ef357d8f4
Pegas-7 <ifk88@mail.ru>
2018-05-21 13:02:47 +0300

    Done task 7

--- src/main/java/task7/Task7.java
+++ src/main/java/task7/Task7.java
@@ -5,8 +5,54 @@ package task7;
 наименьшее и наибольшее число из списка.
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static helpers.HelperMethods.getIntegerNumber;
+
 public class Task7 {
     public static void main(String[] args) {
+        System.out.println("Specify the length of the array");
+        List<Integer> numbers = numberGenerator(getIntegerNumber());
+        System.out.println(numbers);
+        int greatestNumber = seekingGreatestNumber(numbers);
+        int smallestNumber = seekingSmallestNumber(numbers);
 
+        if (greatestNumber == 0 || smallestNumber == 100) {
+            System.out.println("This array doesn't contain something numbers");
+        } else {
+            System.out.println("Greatest number in the array is " + greatestNumber);
+            System.out.println("Smallest number in the array is " + smallestNumber);
+        }
+    }
+
+    public static int seekingGreatestNumber(List<Integer> numbers) {
+        int n = 0;
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) > n) n = numbers.get(i);
+        }
+
+        return n;
+    }
+
+    public static int seekingSmallestNumber(List<Integer> numbers) {
+        int n = 100;
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) < n) n = numbers.get(i);
+        }
+
+        return n;
+    }
+
+    public static List<Integer> numberGenerator(int N) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < N; i++) {
+            list.add((int) (Math.random() * 50 + 50));
+        }
+
+        return list;
     }
 }
